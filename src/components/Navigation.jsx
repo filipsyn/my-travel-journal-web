09ba6891fda47d3a04a9a3195333305819941e5a
Filip <dev.filip0s@protonmail.com>
2022-07-15 21:47:07 +0200

    Added globe icon to `Navigation` component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,8 @@
+import globeIcon from "../assets/globe.svg"
 export default function Navigation() {
     return (
         <nav>
+            <img src={globeIcon} alt="Globe icon"/>
             <h1 className="nav--title">My travel journal</h1>
         </nav>
     )
