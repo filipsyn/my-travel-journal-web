4dd3a4a5fb240d3f93bdb8dd421a260ad16409db
Filip <dev.filip0s@protonmail.com>
2022-07-15 22:16:56 +0200

    Basic layout of `Card` component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Navigation from "./Navigation";
+import CardList from "./CardList";
 
 export default function App() {
     return (
         <div className="app">
             <Navigation />
+            <CardList />
         </div>
     )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import pin from "../assets/location-pin.svg"
+import mountFuji from "../assets/mount-fuji.jpeg"
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img className="card--photo" src={mountFuji} alt=""/>
+            <div className="card--details">
+                <span className="card--location">
+                    <img src={pin} alt="Location pin icon"/>
+                    Location
+                </span>
+                <span className="card--maps-link"><a href="#">View on Google Maps</a></span>
+                <h2 className="card--title">Title</h2>
+                <span className="card--travel-duration">26.4.2022 - 1.5.2022</span>
+                <p className="card--description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at commodi cum cumque, doloribus
+                    expedita nesciunt officia possimus praesentium, quae rem repudiandae sint, voluptate. Labore optio
+                    placeat reprehenderit similique voluptatibus?
+                </p>
+            </div>
+        </div>
+    )
+}
