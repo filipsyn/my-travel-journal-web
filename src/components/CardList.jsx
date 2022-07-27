1f1b4e6d61d9a35b7f79883b14f2c7e778a6a8c5
Filip <dev.filip0s@protonmail.com>
2022-07-27 22:00:47 +0200

    Setup for fetching data from API

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,7 +1,30 @@
 import Card from "./Card";
 import data from "../data/data"
+import {useState, useEffect} from "react";
 
 export default function CardList() {
+    const apiUrl = "https://localhost:7258/api/journal"
+
+    const [cardDataArray, setCardDataArray] = useState([
+            {
+                id: 0,
+                title: "",
+                description: "",
+                location: "",
+                start: "",
+                end: "",
+            }
+        ]
+    )
+
+    useEffect(() => {
+       fetch(apiUrl)
+           .then(res => res.json())
+           .then(data => {
+               console.log(data)
+           })
+    }, [])
+
     const cards = data.map(item => {
         return (
             <Card
